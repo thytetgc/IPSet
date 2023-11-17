3b9d1562b45a3a0e141f9e4c24e44d52c1036037
Thiago Castro <41759383+thytetgc@users.noreply.github.com>
2023-11-17 11:25:49 -0300

    Update update-blacklist.sh

--- ipset/update-blacklist.sh
+++ ipset/update-blacklist.sh
@@ -31,14 +31,14 @@ if [[ ! -d $(dirname "$IP_COUNTRIES") || ! -d $(dirname "$IP_COUNTRIES_RESTORE")
   echo >&2 "Error: missing directory(s): $(dirname "$IP_COUNTRIES" "$IP_COUNTRIES_RESTORE"|sort -u)"
   exit 1
 fi
-
+##########################
 if [[ ! -d $(dirname "$IP_BLACKLIST") || ! -d $(dirname "$IP_BLACKLIST_RESTORE") ]]; then
   echo >&2 "Error: missing directory(s): $(dirname "$IP_BLACKLIST" "$IP_BLACKLIST_RESTORE"|sort -u)"
   exit 1
 fi
 
 ############################################################################################################
-# create the ipset if needed (or abort if does not exists and FORCE=no)
+# crie o ipset se necessário (ou aborte se não existir e FORCE = não)
 if ! ipset list -n|command grep -q "$IPSET_COUNTRIES_NAME"; then
   if [[ ${FORCE:-no} != yes ]]; then
     echo >&2 "Error: ipset does not exist yet, add it using:"
@@ -50,8 +50,8 @@ if ! ipset list -n|command grep -q "$IPSET_COUNTRIES_NAME"; then
     exit 1
   fi
 fi
-
-# create the ipset if needed (or abort if does not exists and FORCE=no)
+##########################
+# crie o ipset se necessário (ou aborte se não existir e FORCE = não)
 if ! ipset list -n|command grep -q "$IPSET_BLACKLIST_NAME"; then
   if [[ ${FORCE:-no} != yes ]]; then
     echo >&2 "Error: ipset does not exist yet, add it using:"
@@ -65,7 +65,7 @@ if ! ipset list -n|command grep -q "$IPSET_BLACKLIST_NAME"; then
 fi
 
 ############################################################################################################
-# create the iptables binding if needed (or abort if does not exists and FORCE=no)
+# crie a ligação iptables se necessário (ou aborte se não existir e FORCE = não)
 if ! iptables -nvL INPUT|command grep -q "match-set $IPSET_COUNTRIES_NAME"; then
   # we may also have assumed that INPUT rule n°1 is about packets statistics (traffic monitoring)
   if [[ ${FORCE:-no} != yes ]]; then
@@ -78,8 +78,8 @@ if ! iptables -nvL INPUT|command grep -q "match-set $IPSET_COUNTRIES_NAME"; then
     exit 1
   fi
 fi
-
-# create the iptables binding if needed (or abort if does not exists and FORCE=no)
+##########################
+# crie a ligação iptables se necessário (ou aborte se não existir e FORCE = não)
 if ! iptables -nvL INPUT|command grep -q "match-set $IPSET_BLACKLIST_NAME"; then
   # we may also have assumed that INPUT rule n°1 is about packets statistics (traffic monitoring)
   if [[ ${FORCE:-no} != yes ]]; then
@@ -97,73 +97,71 @@ fi
 IP_COUNTRIES_TMP=$(mktemp)
 for i in "${COUNTRIES[@]}"
 do
-  IP_TMP=$(mktemp)
-  (( HTTP_RC=$(curl -L -A "blacklist-update/script/github" --connect-timeout 10 --max-time 10 -o "$IP_TMP" -s -w "%{http_code}" "$i") ))
+  IP_TMP_COUNTRIES=$(mktemp)
+  (( HTTP_RC=$(curl -L -A "blacklist-update/script/github" --connect-timeout 10 --max-time 10 -o "$IP_TMP_COUNTRIES" -s -w "%{http_code}" "$i") ))
   if (( HTTP_RC == 200 || HTTP_RC == 302 || HTTP_RC == 0 )); then # "0" because file:/// returns 000
-    command grep -Po '^(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' "$IP_TMP" | sed -r 's/^0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)$/\1.\2.\3.\4/' >> "$IP_COUNTRIES_TMP"
+    command grep -Po '^(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' "$IP_TMP_COUNTRIES" | sed -r 's/^0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)$/\1.\2.\3.\4/' >> "$IP_COUNTRIES_TMP"
     [[ ${VERBOSE:-yes} == yes ]] && echo -n "."
   elif (( HTTP_RC == 503 )); then
-    echo -e "\\nUnavailable (${HTTP_RC}): $i"
+    echo -e "\\nIndisponível (${HTTP_RC}): $i"
   else
-    echo >&2 -e "\\nWarning: curl returned HTTP response code $HTTP_RC for URL $i"
+    echo >&2 -e "\\nAviso: curl retornou código de resposta HTTP $HTTP_RC para URL $i"
   fi
-  rm -f "$IP_TMP"
+  rm -f "$IP_TMP_COUNTRIES"
 done
 
-# sort -nu does not work as expected
+# sort -nu não funciona como esperado
 sed -r -e '/^(0\.0\.0\.0|10\.|127\.|172\.1[6-9]\.|172\.2[0-9]\.|172\.3[0-1]\.|192\.168\.|22[4-9]\.|23[0-9]\.)/d' "$IP_COUNTRIES_TMP"|sort -n|sort -mu >| "$IP_COUNTRIES"
 if [[ ${DO_OPTIMIZE_CIDR} == yes ]]; then
   if [[ ${VERBOSE:-no} == yes ]]; then
-    echo -e "\\nAddresses before CIDR optimization: $(wc -l "$IP_COUNTRIES" | cut -d' ' -f1)"
+    echo -e "\\nEndereços antes da otimização CIDR: $(wc -l "$IP_COUNTRIES" | cut -d' ' -f1)"
   fi
   < "$IP_COUNTRIES" iprange --optimize - > "$IP_COUNTRIES_TMP" 2>/dev/null
   if [[ ${VERBOSE:-no} == yes ]]; then
-    echo "Addresses after CIDR optimization:  $(wc -l "$IP_COUNTRIES_TMP" | cut -d' ' -f1)"
+    echo "Endereços após otimização CIDR:  $(wc -l "$IP_COUNTRIES_TMP" | cut -d' ' -f1)"
   fi
   cp "$IP_COUNTRIES_TMP" "$IP_COUNTRIES"
 fi
-
 rm -f "$IP_COUNTRIES_TMP"
-
+##########################
 IP_BLACKLIST_TMP=$(mktemp)
 for i in "${BLACKLISTS[@]}"
 do
-  IP_TMP=$(mktemp)
-  (( HTTP_RC=$(curl -L -A "blacklist-update/script/github" --connect-timeout 10 --max-time 10 -o "$IP_TMP" -s -w "%{http_code}" "$i") ))
+  IP_TMP_BLACKLIST=$(mktemp)
+  (( HTTP_RC=$(curl -L -A "blacklist-update/script/github" --connect-timeout 10 --max-time 10 -o "$IP_TMP_BLACKLIST" -s -w "%{http_code}" "$i") ))
   if (( HTTP_RC == 200 || HTTP_RC == 302 || HTTP_RC == 0 )); then # "0" because file:/// returns 000
-    command grep -Po '^(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' "$IP_TMP" | sed -r 's/^0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)$/\1.\2.\3.\4/' >> "$IP_BLACKLIST_TMP"
+    command grep -Po '^(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' "$IP_TMP_BLACKLIST" | sed -r 's/^0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)$/\1.\2.\3.\4/' >> "$IP_BLACKLIST_TMP"
     [[ ${VERBOSE:-yes} == yes ]] && echo -n "."
   elif (( HTTP_RC == 503 )); then
-    echo -e "\\nUnavailable (${HTTP_RC}): $i"
+    echo -e "\\nIndisponível (${HTTP_RC}): $i"
   else
-    echo >&2 -e "\\nWarning: curl returned HTTP response code $HTTP_RC for URL $i"
+    echo >&2 -e "\\nAviso: curl retornou código de resposta HTTP $HTTP_RC para URL $i"
   fi
-  rm -f "$IP_TMP"
+  rm -f "$IP_TMP_BLACKLIST"
 done
 
-# sort -nu does not work as expected
+# sort -nu não funciona como esperado
 sed -r -e '/^(0\.0\.0\.0|10\.|127\.|172\.1[6-9]\.|172\.2[0-9]\.|172\.3[0-1]\.|192\.168\.|22[4-9]\.|23[0-9]\.)/d' "$IP_BLACKLIST_TMP"|sort -n|sort -mu >| "$IP_BLACKLIST"
 if [[ ${DO_OPTIMIZE_CIDR} == yes ]]; then
   if [[ ${VERBOSE:-no} == yes ]]; then
-    echo -e "\\nAddresses before CIDR optimization: $(wc -l "$IP_BLACKLIST" | cut -d' ' -f1)"
+    echo -e "\\nEndereços antes da otimização CIDR: $(wc -l "$IP_BLACKLIST" | cut -d' ' -f1)"
   fi
   < "$IP_BLACKLIST" iprange --optimize - > "$IP_BLACKLIST_TMP" 2>/dev/null
   if [[ ${VERBOSE:-no} == yes ]]; then
-    echo "Addresses after CIDR optimization:  $(wc -l "$IP_BLACKLIST_TMP" | cut -d' ' -f1)"
+    echo "Endereços após otimização CIDR:  $(wc -l "$IP_BLACKLIST_TMP" | cut -d' ' -f1)"
   fi
   cp "$IP_BLACKLIST_TMP" "$IP_BLACKLIST"
 fi
-
 rm -f "$IP_BLACKLIST_TMP"
 
 ############################################################################################################
-# family = inet for IPv4 only
+# família = inet apenas para IPv4
 cat >| "$IP_COUNTRIES_RESTORE" <<EOF
 create $IPSET_TMP_COUNTRIES_NAME -exist hash:net family inet hashsize ${HASHSIZE:-16384} maxelem ${MAXELEM:-65536}
 create $IPSET_COUNTRIES_NAME -exist hash:net family inet hashsize ${HASHSIZE:-16384} maxelem ${MAXELEM:-65536}
 EOF
 
-# can be IPv4 including netmask notation
+# pode ser IPv4 incluindo notação de máscara de rede
 # IPv6 ? -e "s/^([0-9a-f:./]+).*/add $IPSET_TMP_COUNTRIES_NAME \1/p" \ IPv6
 sed -rn -e '/^#|^$/d' \
   -e "s/^([0-9./]+).*/add $IPSET_TMP_COUNTRIES_NAME \\1/p" "$IP_COUNTRIES" >> "$IP_COUNTRIES_RESTORE"
@@ -172,14 +170,14 @@ cat >> "$IP_COUNTRIES_RESTORE" <<EOF
 swap $IPSET_COUNTRIES_NAME $IPSET_TMP_COUNTRIES_NAME
 destroy $IPSET_TMP_COUNTRIES_NAME
 EOF
-
-# family = inet for IPv4 only
+##########################
+# família = inet apenas para IPv4
 cat >| "$IP_BLACKLIST_RESTORE" <<EOF
 create $IPSET_TMP_BLACKLIST_NAME -exist hash:net family inet hashsize ${HASHSIZE:-16384} maxelem ${MAXELEM:-65536}
 create $IPSET_BLACKLIST_NAME -exist hash:net family inet hashsize ${HASHSIZE:-16384} maxelem ${MAXELEM:-65536}
 EOF
 
-# can be IPv4 including netmask notation
+# pode ser IPv4 incluindo notação de máscara de rede
 # IPv6 ? -e "s/^([0-9a-f:./]+).*/add $IPSET_TMP_BLACKLIST_NAME \1/p" \ IPv6
 sed -rn -e '/^#|^$/d' \
   -e "s/^([0-9./]+).*/add $IPSET_TMP_BLACKLIST_NAME \\1/p" "$IP_BLACKLIST" >> "$IP_BLACKLIST_RESTORE"
@@ -191,15 +189,13 @@ EOF
 
 ############################################################################################################
 ipset -file  "$IP_COUNTRIES_RESTORE" restore
-
 if [[ ${VERBOSE:-no} == yes ]]; then
   echo
-  echo "Blacklisted addresses found: $(wc -l "$IP_COUNTRIES" | cut -d' ' -f1)"
+  echo "Endereços na lista negra encontrados: $(wc -l "$IP_COUNTRIES" | cut -d' ' -f1)"
 fi
-
+##########################
 ipset -file  "$IP_BLACKLIST_RESTORE" restore
-
 if [[ ${VERBOSE:-no} == yes ]]; then
   echo
-  echo "Blacklisted addresses found: $(wc -l "$IP_BLACKLIST" | cut -d' ' -f1)"
+  echo "Endereços na lista negra encontrados: $(wc -l "$IP_BLACKLIST" | cut -d' ' -f1)"
 fi
